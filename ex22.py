a073669ccb783195e39d455344bae19bb5cc7b6b
谷立东 <gld@gulidongdeMacBook-Pro.local>
2020-09-16 23:06:02 +0800

    9.16

---
+++ ex22.py
@@ -0,0 +1,35 @@
+print
+#
++,-,/,*,%,<,>,<=,>=
+variable_variable
+f"String {somevar}"
+.format()
+print("." * 10)
+print("""
+String
+""")
+转义 ' " \
+input()
+input(var)
+y = input("Name? ")
+"{} {}".format(x, y)
+pydoc 
+from sys import argv
+var1, var2 = argv
+open(filename)
+txt.read()
+txt.close()
+.readline()
+.truncate()
+.write('stuff')
+.seek(0)
+r,w,a
+r+, w+, a+
+from os.path import exists
+exists(var) return True, False
+def function(*args):
+def function(arg1, arg2):
+print("string" + end='')
+return
+
+
